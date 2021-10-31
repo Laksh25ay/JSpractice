419aeb9ec6924cd577ab6724f086b6855fd862ef
lakshay <lakshay250407@gmail.com>
2021-10-31 23:34:11 +0530

    Implemeted Jokes project using async await as well

--- AdvanceJS/Projects/Project1_AJAXXMLHttpRequest/jokes.js
+++ AdvanceJS/Projects/Project1_AJAXXMLHttpRequest/jokes.js
@@ -2,15 +2,33 @@ const targetEle = document.querySelector(".targetEle");
 const getJoke = document.querySelector(".getJoke");
 
 
+//using promises
 
-function getJokes() {
-    fetch("https://icanhazdadjoke.com/", {
-        headers: {
-            Accept: "application/json"
-        }
-    }).then((res) => res.json())
-        .then((data) => targetEle.innerHTML = data.joke)
-        .catch((err) => console.log("err"));
+// function getJokes() {
+//     fetch("https://icanhazdadjoke.com/", {
+//         headers: {
+//             Accept: "application/json"
+//         }
+//     }).then((res) => res.json())
+//         .then((data) => targetEle.innerHTML = data.joke)
+//         .catch((err) => console.log("err"));
+// }
+
+
+//Using async await
+async function getJokes(){
+    try{
+        const res = await fetch("https://icanhazdadjoke.com/", {
+            headers: {
+                Accept: "application/json"
+            }
+        });
+        const data = await res.json();
+        targetEle.innerHTML = data.joke;
+
+    }catch(err){
+        console.log(err);
+    }
 }
 
 getJoke.addEventListener("click", getJokes);
